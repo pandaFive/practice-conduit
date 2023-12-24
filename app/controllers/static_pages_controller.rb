4b67a52635fc7721f4c4cfd3e55fcecd2e1daf20
propan <ldspdk5v@gmail.com>
2023-12-24 23:14:02 +0900

    create home paginate link

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @articles = Article.all.paginate(page: params[:page], per_page: 10)
+    @custom_renderer = CustomLinkRenderer.new
   end
 end
