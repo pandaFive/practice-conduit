5c232eeff06ade8e262c99cd6169deed972e9be2
propan <ldspdk5v@gmail.com>
2023-12-24 21:39:37 +0900

    create show article

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ authors = Author.order(:created_at).take(5)
 50.times do |n|
   title = Faker::Lorem.word
   description = Faker::Lorem.sentence(word_count: 5)
-  content = Faker::Lorem.sentences(number: 50)
+  content = Faker::Lorem.paragraphs(number: 10).join("")
 
   authors.each { |author| author.articles.create!(title: title, description: description, content: content) }
 end
